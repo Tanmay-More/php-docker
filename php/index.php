be8629cbf0259c699c597e21bdac5d5e1469b3de
Tanmay-More <tanmay.more@avilasinfotech.in>
2022-03-28 14:54:29 +0530

    sample change 1

--- php/index.php
+++ php/index.php
@@ -8,6 +8,7 @@
 <body>
     <?php
         echo "<h1 style='text-align: center; color: green;'>Hello world..!!</h1>";
+        echo "<h1 style='text-align: center; color: green;'>Hello world..!!</h1>";
         echo "<br><p>This is just a sample text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga et aspernatur sit dignissimos cum ratione obcaecati consectetur placeat a, id illum magnam eum sequi odit unde pariatur assumenda deserunt aliquam.</p>";
         echo "<br><p>This is just a <i>sample text</i>. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga et <strong style='color: red'>aspernatur</strong> sit dignissimos cum ratione obcaecati consectetur placeat a, id illum magnam eum sequi odit unde pariatur assumenda deserunt aliquam.</p>";
         echo "<br><p>This is just a sample text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga et aspernatur sit dignissimos cum ratione <strong style='color: red'>obcaecati</strong> consectetur placeat a, id illum magnam eum sequi odit unde pariatur assumenda deserunt aliquam. This is just a sample text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga et aspernatur sit dignissimos cum ratione obcaecati consectetur placeat a, id illum magnam eum sequi odit unde pariatur assumenda deserunt aliquam.</p>";
