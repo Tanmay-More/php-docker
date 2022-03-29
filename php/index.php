a7029ffe974be48845163fd4615d150d45a989d5
Tanmay-More <tanmay.more@avilasinfotech.in>
2022-03-29 16:26:17 +0530

    sample change

--- php/index.php
+++ php/index.php
@@ -9,5 +9,6 @@
     <?php
         echo "<h1 style='text-align: center; color: green; margin: 30px auto'>Hello world..!!</h1>";
     ?>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet facilis ipsa cum magnam officiis ab odio obcaecati nemo sit quidem officia tempora, quaerat ut? Vero error non hic delectus deserunt.</p>
 </body>
 </html>
